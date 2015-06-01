1d9cff9904a2385168fe63bf1fbdae3d07427a03
wonshin <shin.won.ee@gmail.com>
2015-06-01 22:58:04 +0900

    Model_Working_for_2014_WTG01_result

--- codes/NormalBehaviourModel.R
+++ codes/NormalBehaviourModel.R
@@ -1,107 +1,106 @@
-library(neuralnet)
-source('./codes/dataloader.R')
 
-s <- scada('WTG01', '2014')
-s <- scada('WTG01', '2009')
+### ANN modeling ###
 
-windScada.2009 <- s$data()
-windScada.early <- head(windScada, n=10000)
-plot(windScada.early$Gear.Bearing.Temperature.Average)
-windScada.2014 <- s$data()
-windScada.late <- windScada[15000:40000,]
-plot(windScada.late$Gear.Bearing.Temperature.Average)
+# data loading from SCADA csv file 
+dataloading <- function (turbine='WTG01', yr='2014',start=0, end=10000) {
 
-# won's suggested model
-input.oil.temp <- windScada.early$Gear.Oil.Temperature.Average
-# input.oil.temp.t1 <- windScada.early$Gear.Oil.Temperature.Average
-# input.bearing.temp.t1 <- windScada.early$Gear.Bearing.Temperature.Average
-# input.bearing.temp.t2 <- windScada.t$Gear.Bearing.Temperature.Average
-input.power <- windScada.early$Grid.Production.Power.Average
-# input.power.t1 <- windScada.t1$Grid.Production.Power.Average
-input.ambient.temp <-windScada.early$Ambient.Temperature.Average
-output.bearing.temp <- windScada.early$Gear.Bearing.Temperature.Average
+  source('./codes/dataloader.R')
+  s <- scada('WTG01', '2014')
+  # Remove the data when power is below zero
+  windScada <- s$data()
+  windScada <- subset(windScada,windScada$Grid.Production.Power.Average>0)[start:end,] 
+  
+  oil.temp <- windScada$Gear.Oil.Temperature.Average
+  power <- windScada$Grid.Production.Power.Average
+  ambient.temp <-windScada$Ambient.Temperature.Average
+  bearing.temp <- windScada$Gear.Bearing.Temperature.Average
+  
+  data<-data.frame(oil.temp,power,ambient.temp,bearing.temp)
+}
 
-input.oil.temp.late <- windScada.late$Gear.Oil.Temperature.Average
-# input.oil.temp.t1 <- windScada.early$Gear.Oil.Temperature.Average
-# input.bearing.temp.t1 <- windScada.early$Gear.Bearing.Temperature.Average
-# input.bearing.temp.t2 <- windScada.t$Gear.Bearing.Temperature.Average
-input.power.late <- windScada.late$Grid.Production.Power.Average
-# input.power.t1 <- windScada.t1$Grid.Production.Power.Average
-input.ambient.temp.late <-windScada.late$Ambient.Temperature.Average
-output.bearing.temp.late <- windScada.late$Gear.Bearing.Temperature.Average
-
-
-#  create training dataset
-train.data <- data.frame( x1=input.oil.temp,
-                          #                          x2=input.bearing.temp.t1,
-                          #                          x3=input.bearing.temp.t2,
-                          x4=input.power,
-                          x5=input.ambient.temp,
-                          y=output.bearing.temp )
-
-test.data <- data.frame( x1=input.oil.temp.late,
-                         #                           x2=input.bearing.temp.t1,
-                         #                           x3=input.bearing.temp.t2,
-                         x4=input.power.late,
-                         x5=input.ambient.temp.late,
-                         y=output.bearing.temp.late)
-
-# To narrow the range, filter-out the data when power is below XX 
-#test.data <- subset(test.data,test.data$x4>0)
-train.data.filtered <- subset(train.data,train.data$x4>0)
-test.data.filtered <- subset(test.data,train.data$x4>0)
-plot(x4~y,train.data.filtered)
-pairs(train.data.filtered)
-plot(x1~y,train.data.filtered)# TODO normalize training data
-
-weights <- 27 # TODO What will be a right weights value?
-hidden.layer.size <- 3 # TODO What will be a right size?
-
+# Prepare training data
+training.data<-dataloading('WTG01','2014',0,1000)
 
 # Training with Neural Network
 library(nnet)
-#ann.model <- nnet(formula, train.data, weights, size=hidden.layer.size)
-#seed.val <- 2
-hidden.layer.size <- 10 # TODO What will be a right size?
-#set.seed(seed.val)
-ann.model <- nnet(y ~x1+x4+x5, train.data.filtered, size=hidden.layer.size,linout=T, maxit = 10000,threshold=0.01)
+hidden.layer.size <- 9 
+ann.model <- nnet(bearing.temp ~ oil.temp+power+ambient.temp, training.data, size=hidden.layer.size,linout=T, maxit = 10000,threshold=0.01)
 #ann.model <- neuralnet(y~x1+x2+x3+x4+x5,train.data.filtered,hidden=10,threshold=0.01)
 print(ann.model)
-# plotting with plot.nnet
-#library(clusterGeneration)
-#library(devtools)
 
+## @brief plotting with plot.nnet ###
+library(clusterGeneration)
+library(devtools)
 #import the function from Github
-#source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
-#plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
+source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
+plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
 
 
-# prediction with test data
-# TODO create test dataset
-#train.data.filtered<-train.data.filtered[1:1000,]
+## Testing ## 
+#
+#
+test.data<-dataloading('WTG01','2014',1001,2000)
+test.expected.output<-test.data[,4]
+test.data<-test.data[,1:3]
+test.nnet.result<-data.frame(predict(ann.model, newdata=test.data,type="raw"))
+test.residual<-test.expected.output-test.nnet.result
+print(cbind(test.expected.output,test.nnet.result))
 
-#create test data from training data itself 
-datasize <- length(test.data.filtered$y)
-test.data <- cbind(test.data.filtered[1:datasize,1],test.data.filtered[1:datasize,2],test.data.filtered[1:datasize,3])
-colnames(test.data)<-c("x1","x4","x5")
-#test.data <- cbind(train.data.filtered$x1,train.data.filtered$x2,train.data.filtered$x3,train.data.filtered$x4,train.data.filtered$x5)
-#colnames(test.data)<-c("x1","x2","x3","x4","x5")
-
-#predict 
-nnet.result<-predict(ann.model, newdata=test.data,type="raw")
-#nnet.result<-compute(ann.model, test.data$x1+test.data$x2+test.data$x3+test.data$x4+test.data$x5)
-result <- cbind(test.data.filtered[1:datasize,4],as.data.frame(nnet.result))
-colnames(result)<-c("Expected Output","Neural Net Output")
-print(result)
-result<-data.frame(nnet.result)
-residual<-result-test.data.filtered[1:datasize,4]
-x<-data.frame(seq(1,datasize,1))
-resultplot<-cbind(x,residual)
+# plotting
+x<-data.frame(seq(1,length(test.residual[,1])))
+test.resultplot<-cbind(x,test.residual)
+colnames(test.resultplot)<-c("x","residual")
 par(mfrow=c(2,1))
-plot(nnet.result,type="l",col="red")
-lines(test.data.filtered[1:datasize,4],col="green")
-plot(resultplot,type="l")
-#test<-data.frame(test.data$y)
-#plot(test,result)
-summary(residual)
+plot(test.nnet.result[,1],type="l",col="red",xlab='sample point',ylab='bearing temperture',ylim=range(40,80))
+lines(test.expected.output,col="green")
+plot(test.resultplot,type="p",xlab='sample point',ylab='residual',pch=20,ylim=range(-10,10))
+par(fig=c(0.65,1.0,0,0.5),new=TRUE)
+boxplot(test.residual,axes=FALSE,ylim=range(-10,10))
+
+library(zoo)
+#Make zoo object of data
+test.temp.zoo<-zoo(test.resultplot[,2])
+#Calculate moving average with window 3 and make first and last value as NA (to ensure identical length of vectors)
+test.m.av<-rollmean(test.temp.zoo, 50,fill = list(NA, NULL, NA))
+#Add calculated moving averages to existing data frame
+test.resultplot$amb.av=coredata(test.m.av)
+#Add additional line for moving average in black
+library(ggplot2)
+library(scales)
+ggplot(test.resultplot, aes(x, residual)) + geom_point(colour="green") + geom_line(aes(x,amb.av),color="black")
+
+## Prediction ## 
+#
+#
+predict.data<-dataloading('WTG01','2014',15001,20000)
+predict.expected.output<-predict.data[,4]
+predict.data<-predict.data[,1:3]
+predict.nnet.result<-data.frame(predict(ann.model, newdata=predict.data,type="raw"))
+predict.residual<-predict.expected.output-predict.nnet.result
+print(cbind(predict.expected.output,predict.nnet.result))
+
+# plotting
+x<-data.frame(seq(1,length(predict.residual[,1])))
+predict.resultplot<-cbind(x,predict.residual)
+colnames(predict.resultplot)<-c("x","residual")
+par(mfrow=c(2,1))
+plot(predict.nnet.result[,1],type="l",col="red",xlab='sample point',ylab='bearing temperture',ylim=range(40,80))
+lines(predict.expected.output,col="green")
+plot(predict.resultplot,type="p",xlab='sample point',ylab='residual',pch=20,ylim=range(-10,10))
+par(fig=c(0.65,1.0,0,0.5),new=TRUE)
+boxplot(predict.residual,axes=FALSE,ylim=range(-10,10))
+
+
+library(zoo)
+#Make zoo object of data
+temp.zoo<-zoo(predict.resultplot[,2])
+#Calculate moving average with window 3 and make first and last value as NA (to ensure identical length of vectors)
+m.av<-rollmean(temp.zoo, 100,fill = list(NA, NULL, NA))
+#Add calculated moving averages to existing data frame
+predict.resultplot$amb.av=coredata(m.av)
+#Add additional line for moving average in red
+library(ggplot2)
+library(scales)
+library(reshape2)
+ggplot(predict.resultplot, aes(x, residual)) + geom_point(colour="green") + geom_line(aes(x,amb.av),color="black")
 
