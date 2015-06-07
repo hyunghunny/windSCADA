f07ffbbf3fcdf5f149bb51ed531c28369aa12012
wonshin <shin.won.ee@gmail.com>
2015-06-07 18:19:42 +0900

    Prediction function added

--- codes/NormalBehaviourModel_Vattenfall.R
+++ codes/NormalBehaviourModel_Vattenfall.R
@@ -2,23 +2,6 @@
 ### ANN modeling ###
 
 # data loading from SCADA csv file 
-dataloading <- function (turbine='WTG01',yr='2014',start=1) {
-
-  source('./codes/dataloader.R')
-  s <- scada(turbine,yr)
-  # Remove the data when power is below zero
-  windScada <- s$data()
-  s.length<-length(windScada$Gear.Oil.Temperature.Average)
-  windScada <- subset(windScada,windScada$Grid.Production.Power.Average>0)#[start:s.length,] 
-  
-  time <- windScada$PCTimeStamp
-  oil.temp <- windScada$Gear.Oil.Temperature.Average
-  power <- windScada$Grid.Production.Power.Average
-  ambient.temp <-windScada$Ambient.Temperature.Average
-  bearing.temp <- windScada$Gear.Bearing.Temperature.Average
-  
-  data<-data.frame(time,oil.temp,power,ambient.temp,bearing.temp)
-}
 
 ##Vattenfall Data Loading##
 
@@ -27,13 +10,13 @@ dataloading <- function (turbine='WTG01',yr='2014',start=1,end) {
   windScada <- read.csv("./data/WH1581.csv")
   # Remove the data when power is below zero
   s.length<-length(windScada$WH1581.GearBearTempAvg)
-  windScada <- subset(windScada,WH1581.ActivePower>0)[start:end] 
+  windScada <- subset(windScada,windScada$WH1581.ActivePower>0)[start:end,] 
   
-  time <- windScada$WH1581.UTC.Timestamp
+  time <- windScada$UTC.Timestamp
   oil.temp <- windScada$WH1581.GearOilTempAvg
   power <- windScada$WH1581.ActivePower
   ambient.temp <-windScada$WH1581.NacelleTempAvg
-  bearing.temp <- windScada$GearBearTempAvg
+  bearing.temp <- windScada$WH1581.GearBearTempAvg
   
   data<-data.frame(time,oil.temp,power,ambient.temp,bearing.temp)
 }
@@ -44,33 +27,33 @@ library(devtools)
 library(reshape)
 
 # Prepare training data
-bearing.training <- function(turbine,yr,start)
-  {
-  training.data<-dataloading(turbine,yr,start)
+bearing.training <- function(turbine,yr,start,end)
+{
+  training.data<-dataloading(turbine,yr,start,end)
   hidden.layer.size <- 9 
   ann.model <- nnet(bearing.temp ~ oil.temp+power+ambient.temp, training.data, size=hidden.layer.size,linout=T, maxit = 10000,threshold=0.01)
   #ann.model <- neuralnet(y~x1+x2+x3+x4+x5,train.data.filtered,hidden=10,threshold=0.01)
   print(ann.model)
   ## @brief plotting with plot.nnet ###
-#   #import the function from Github
-#   source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
-#   plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
-
+  #   #import the function from Github
+  #   source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
+  #   plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
+  
   return(ann.model)
 }
 
-predict.bearing <- function (turbine, yr,start) {
-#   
-#   turbine='WTG01' 
-#   yr='2014'
-#   start=0
-#   end=10000
+predict.bearing <- function (turbine, yr,start,end) {
+  #   
+  #   turbine='WTG01' 
+  #   yr='2014'
+  #   start=0
+  #   end=10000
   library(zoo)
   library(ggplot2)
   library(scales)
   library(reshape2)
-
-  predict.data<-dataloading(turbine,yr,start)
+  
+  predict.data<-dataloading(turbine,yr,start,end)
   predict.expected.output<-predict.data$bearing.temp
   predict.data<-predict.data[,1:4]
   predict.nnet.result<-data.frame(predict(ann.model, newdata=predict.data,type="raw"))
@@ -78,7 +61,7 @@ predict.bearing <- function (turbine, yr,start) {
   #Add calculated moving averages to existing data frame
   output <- cbind(predict.data[,1],predict.residual,rollmean(predict.residual, 100,fill = list(NA, NULL, NA)),predict.expected.output,predict.nnet.result)
   colnames(output) <- c('Time','Residual','amb.av','Measured','Predicted')
-#   print(output)
+  #   print(output)
   
   # plotting
   par(mfrow=c(2,1))
@@ -94,15 +77,16 @@ predict.bearing <- function (turbine, yr,start) {
   #Calculate moving average with window 3 and make first and last value as NA (to ensure identical length of vectors)
   #Add additional line for moving average in red
   print(ggplot(output,aes(Time,Residual)) + geom_point(colour="green") + geom_line(aes(Time,amb.av),color="black")+labs(title=paste(turbine,'Bearing Temperature Residual from',output$Time[1],'to',output$Time[length(output$Time)])) + ylim(-10,10))
-
+  
   return(list(turbine=turbine,year=yr,output=output))
-
+  
 }
 
-ann.model<-bearing.training('WTG04','2009',1)
+ann.model<-bearing.training('WTG04','2009',1,5000)
+Vattenfall.Output2010<-predict.bearing(turbine='WTG04', yr='2010',start=5000,10000)
+
 WTG04.Output2010<-predict.bearing(turbine='WTG04', yr='2010',start=1)
 WTG04.Output2011<-predict.bearing(turbine='WTG04', yr='2011',start=1)
 WTG04.Output2012<-predict.bearing(turbine='WTG04', yr='2012',start=1)
 WTG04.Output2013<-predict.bearing(turbine='WTG04', yr='2013',start=1)
 WTG04.Output2014<-predict.bearing(turbine='WTG04', yr='2014',start=1)
-  
