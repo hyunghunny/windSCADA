d98349d79fd050032afe84c972feb1c4a25ff2e2
hyunghunny <hyunghunny@gmail.com>
2015-06-08 09:56:11 +0900

    fix bug: error after removing (a) column(s) at dataloader PCA for selected parameters

--- codes/PCA_exercise.R
+++ codes/PCA_exercise.R
@@ -1,30 +1,34 @@
+# load data
+source('./codes/dataloader.R')
 
 ##
-# PCA runner for wind SCADA data set
-#
-pca <- function(windturbine.id, year) {  
-  
-  # load data
-  source('./codes/dataloader.R')
+#' linear PCA analysis for wind SCADA data set
+#' @param s wind SCADA object by data loader
+#' 
+pca <- function(s) {  
+
   # run PCA and show bi-plots
   run <- function() {  
-    # data loading by module 
-    s <- scada(windturbine.id, toString(year))
+
     
-    windScada.all = s$data()
+    # windScada.all = s$data()
     
     # clustering by category
     windScada.control <- s$control()
+    #windScada.control <- scale(windScada.control,center=F,scale=T) # normalization
     #names(windScada.control)
     
     windScada.env <- s$env()
+    windScada.env <- scale(windScada.env,center=F,scale=T) # normalization
     #names(windScada.env)
     
     windScada.mech <- s$mech()
+    windScada.mech <- scale(windScada.mech,center=F,scale=T) # normalization
     #names(windScada.mech)
     
     
     windScada.power <- s$power()
+    #windScada.power <- scale(windScada.power,center=F,scale=T) # normalization
     #names(windScada.power)
     
     # Principal Component Analysis
@@ -33,28 +37,41 @@ pca <- function(windturbine.id, year) {
     pr.env <- princomp(windScada.env, scale=TRUE) 
     pr.mech <- princomp(windScada.mech, scale=TRUE) 
     pr.power <- princomp(windScada.power, scale=TRUE) 
+  }
+  
+  
+  run4paper <- function() {
+    windScada.simap <- s$simap()
     
+    # normalization
+    windScada.simap <- scale(windScada.simap,center=F,scale=T)
+    pr.simap <- princomp(windScada.simap, scale=TRUE)
     
-    par(mfrow = c(2, 2)) 
+    biplot(pr.simap, cex=0.5, main='PCA of SCADA Parameters', col=c("grey","red"), pch=20)
+    return (pr.simap)
+  }
+  
+  draw <- function(title='', row=1, col=1) { 
+    
+    par(mfrow = c(row, col)) 
     #summary(pr.control)
-    biplot(pr.control, cex=0.5, main=paste(windturbine.id, 'Control PCA', '(', year, ')'))
-    #warnings()
+    biplot(pr.control, cex=0.5, main=paste(title, 'Control PCA'), col=c("grey","red"))
     #pca.control = pr.control$scores[, 1] # Contains 0.99 Cumulative Proposition
         
     #summary(pr.env)
-    biplot(pr.env, cex=0.5, main=paste(windturbine.id, 'Env. PCA', '(', year, ')'))
-    #warnings()
+    biplot(pr.env, cex=0.5, main=paste(title, 'Env. PCA'), col=c("grey","red"))
     #pca.env = pr.env$scores[, 1:3] # Contains 0.99 Cumulative Proposition
     
     #summary(pr.mech)
-    biplot(pr.mech, cex=0.5, main=paste(windturbine.id, 'Mech. PCA', '(', year, ')'))
-    #warnings()
+    biplot(pr.mech, cex=0.5, main=paste(title, 'Mech. PCA'), col=c("grey","red"))
     #pca.mech = pr.mech$scores[, 1:9] # Contains 0.99 Cumulative Proposition
     
     #summary(pr.power)
-    biplot(pr.power, cex=0.5, main=paste(windturbine.id, 'Power PCA', '(', year, ')'))
+    biplot(pr.power, cex=0.5, main=paste(title, 'Power PCA'), col=c("grey","red"))
     #warnings()
     #pca.power = pr.power$scores[, 1:2]
+    
+    par(mfrow = c(1, 1)) 
   }
   
   # returns control principle components
@@ -77,9 +94,28 @@ pca <- function(windturbine.id, year) {
     return (pr.power)
   }
   
-  return (list(run=run, control=control, env=env, mech=mech, power=power))
+  return (list(run=run, draw=draw, control=control, env=env, mech=mech, power=power, run4paper=run4paper))
 }
 
+#' Non linear PCA
+#'
+nlpca <- function(data) {
+  library(pcaMethods)  
+  
+  nlpca.data <- prep(data, scale="none", center=TRUE)
+  
+  run <- function () {
+    # TODO: add code to run NLPCA
+    
+    resNLPCA <- pca(nlpca.data, method="nlpca", center=FALSE, nPcs=5, maxSteps=300)
+    
+    
+    # homogeneity analysis
+    #nlpca.obj <- homals(nlpca.data) # XXX: it returns 'Error in sort.list(y) : 'x' must be atomic for 'sort.list''
+  }
+  
+  return (list(run=run))
+}
 
 ##
 # draw PCA plots for all years of a specific wind turbine.
@@ -90,9 +126,16 @@ pca <- function(windturbine.id, year) {
 pca_years <- function (windturbine.id, from, to) {
   for (year in seq(from, to)) {
     print (toString(year))
-    pca.runner <- pca(windturbine.id, year)
+    
+    # data loading by module 
+    s <- scada(windturbine.id, toString(year))    
+    pca.runner <- pca(s)
     
     pca.runner$run()
+    
+    title <- paste(year, windturbine.id)
+    
+    pca.runner$draw(title, 2, 2)
     # exception handling if error occurred
     #tryCatch(pca.runner$run(), error=function(err)  print "error", finally=print "finished" )    
   }
@@ -100,4 +143,12 @@ pca_years <- function (windturbine.id, from, to) {
 
 # PCA plotting of a specific wind turbine for years
 
-pca_years("WTG02", 2010, 2014)
+pca_years("WTG01", 2014, 2014)
+
+#s <- scada("WTG01", 2014)    
+#pca.runner <- pca(s)
+
+#pr.simap <- pca.runner$run4paper()
+#summary(pr.simap)
+#unclass(pr.simap$loadings)
+
--- codes/dataloader.R
+++ codes/dataloader.R
@@ -1,10 +1,13 @@
-##
-# SCADA data loader by module pattern
-# @param turbine.id turbine ID
-# @param year year of measurement
 #
-# @comment by default, WTG01/2014 will be used.
-#
+#' Wind SCADA data loader by module pattern
+#' 
+#' @param turbine.id turbine ID
+#' @param year year of measurement
+#' 
+#' @description 
+#' \code{scala} returns an object which returns data
+#' @details by default, WTG01/2014 will be used.
+#'
 scada <- function (turbine.id='WTG01', year='2014') {
   
   # concanterate strings for making csv file path
@@ -21,12 +24,7 @@ scada <- function (turbine.id='WTG01', year='2014') {
     converted <- strptime(scada.data$PCTimeStamp, "%Y-%m-%d %H:%M:%S")
   } 
   scada.data$PCTimeStamp <- converted  
-  
-  # remove unused column
-  drops <- c("Number.of.currently.main.wind.senor..1.2.")
-  scada.data <- scada.data[, !(names(scada.data) %in% drops)]
-  
-  
+    
   # dataset ordering
   scada.data <- scada.data[order(scada.data$PCTimeStamp), ]
   
@@ -79,18 +77,23 @@ scada <- function (turbine.id='WTG01', year='2014') {
   
 
   
-  # return data title
+  #' Return data title
   title <- function() {
     return (scada.title)
   }
   
-  # returns full dataset with order by PCTimeStamp
+  #' Returns full dataset with order by PCTimeStamp
   data <- function () {
-    return (scada.data)
+    # set unused columns 
+    scada.drops <- c("Number.of.currently.main.wind.senor..1.2.")  
+    return.data <- scada.data[, !(names(scada.data) %in% scada.drops)]
+    
+    return (return.data)
   }
   
-  # return monthly subset data
-  # @param Number 1~12 represents janunary to december
+  #' Return monthly subset data
+  #' @param Number 1~12 represents janunary to december
+  #' @return dataset of the specific month 
   month <- function(m) {
     if (m == 1) {
       return (scada.data.jan)
@@ -130,35 +133,65 @@ scada <- function (turbine.id='WTG01', year='2014') {
     }    
   }
   
-  # returns size of data rows 
+  #' Returns size of data rows 
   size <- function () {
     return (scada.data.size)
   }
   
-  # returns time series
+  #' Returns time series
   date <- function () {
     return (as.Date(scada.data$PCTimeStamp, "%Y-%m-%d"))
   }
   
-  # returns control associated features only
+  #' Returns control associated features only
   controls <- function () {
     return (scada.data[, c(11:14, 28:31, 69:100, 123:126, 132)])
   }
   
-  # returns environment associated features only
+  #' Returns environment associated features only
   envs <- function () {
     return (scada.data[, c(3:10, 106)])
   }
   
-  # returns mechanic assoicated features only
+  #' Returns mechanic assoicated features only
   mechs <- function () {
     return (scada.data[, c(15:26, 32:34, 66:68, 101:104, 127:128)])
   }
   
-  # returns power associated features only
+  #' Returns power associated features only
   powers <- function () {
     return (scada.data[, c(35:65, 107:122)])
-  }  
+  }
   
-  return (list(title=title, data=data, month=month, size=size, date=date, control=controls, env=envs, mech=mechs, power=powers))
+  simap <- function() {
+    # inputs which were selected at the paper 'Online Wind Turbine Fault Detection through Automated SCADA Data Analysis
+        
+    power <- scada.data$Grid.Production.Power.Average # XXX: wonder that it may be correct...
+    
+    active.power <- scada.data$Total.active.power
+    wind.speed.avg <- scada.data$Ambient.WindSpeed.Average
+    wind.speed.stdev <- scada.data$Ambient.WindSpeed.StdDev
+    
+    nacelle.temp <- scada.data$Nacelle.Temperature.Average
+    bearing.temp <- scada.data$Gear.Bearing.Temperature.Average
+    oil.temp <- scada.data$Gear.Oil.Temperature.Average
+    
+    ambient.temp <-scada.data$Ambient.Temperature.Average
+    
+    # XXX: no generator winding argument in SCADA data
+    power.factor <- scada.data$Power.factor.set.point # XXX: it is an ambiguous one.
+    reactive.power <- scada.data$Total.reactive.power
+    
+    # following are phase currents
+    phase.current1 <- scada.data$Grid.Production.CurrentPhase1.Average
+    phase.current2 <- scada.data$Grid.Production.CurrentPhase2.Average
+    phase.current3 <- scada.data$Grid.Production.CurrentPhase3.Average
+    
+    return.data <- data.frame(power, active.power, wind.speed.avg, wind.speed.stdev, 
+                              nacelle.temp, bearing.temp, oil.temp, ambient.temp, power.factor, reactive.power,
+                              phase.current1, phase.current2, phase.current3)
+    return (return.data)
+  }
+  
+  return (list(title=title, data=data, month=month, size=size, date=date, control=controls, env=envs, mech=mechs, power=powers, simap=simap))
 }
