15b7cb6f699f5f0215b5e896059c0afbb2327636
wonshin <shin.won.ee@gmail.com>
2015-06-08 18:50:12 +0900

    Final

--- codes/NormalBehaviourModel.R
+++ codes/NormalBehaviourModel.R
@@ -2,14 +2,14 @@
 ### ANN modeling ###
 
 # data loading from SCADA csv file 
-dataloading <- function (turbine='WTG01',yr='2014',start=1) {
+dataloading <- function (turbine='WTG01',yr='2014',start=1,end=s.length) {
 
   source('./codes/dataloader.R')
   s <- scada(turbine,yr)
   # Remove the data when power is below zero
   windScada <- s$data()
   s.length<-length(windScada$Gear.Oil.Temperature.Average)
-  windScada <- subset(windScada,windScada$Grid.Production.Power.Average>0)#[start:s.length,] 
+  windScada <- subset(windScada,windScada$Grid.Production.Power.Average>0)[start:end,] 
   
   time <- windScada$PCTimeStamp
   oil.temp <- windScada$Gear.Oil.Temperature.Average
@@ -18,25 +18,9 @@ dataloading <- function (turbine='WTG01',yr='2014',start=1) {
   bearing.temp <- windScada$Gear.Bearing.Temperature.Average
   
   data<-data.frame(time,oil.temp,power,ambient.temp,bearing.temp)
+  data<-na.omit(data)
 }
 
-##Vattenfall Data Loading##
-
-dataloading <- function (turbine='WTG01',yr='2014',start=1,end) {
-  
-  windScada <- read.csv("./data/WH1581.csv")
-  # Remove the data when power is below zero
-  s.length<-length(windScada$WH1581.GearBearTempAvg)
-  windScada <- subset(windScada,WH1581.ActivePower>0)[start:end] 
-  
-  time <- windScada$WH1581.UTC.Timestamp
-  oil.temp <- windScada$WH1581.GearOilTempAvg
-  power <- windScada$WH1581.ActivePower
-  ambient.temp <-windScada$WH1581.NacelleTempAvg
-  bearing.temp <- windScada$GearBearTempAvg
-  
-  data<-data.frame(time,oil.temp,power,ambient.temp,bearing.temp)
-}
 # Training with Neural Network
 library(nnet)
 library(clusterGeneration)
@@ -49,45 +33,53 @@ bearing.training <- function(turbine,yr,start)
   training.data<-dataloading(turbine,yr,start)
   hidden.layer.size <- 9 
   ann.model <- nnet(bearing.temp ~ oil.temp+power+ambient.temp, training.data, size=hidden.layer.size,linout=T, maxit = 10000,threshold=0.01)
-  #ann.model <- neuralnet(y~x1+x2+x3+x4+x5,train.data.filtered,hidden=10,threshold=0.01)
+# ann.model <- nnet(bearing.temp ~ power+ambient.temp, training.data, size=hidden.layer.size,linout=T, maxit = 10000,threshold=0.01)
+#ann.model <- neuralnet(y~x1+x2+x3+x4+x5,train.data.filtered,hidden=10,threshold=0.01)
   print(ann.model)
   ## @brief plotting with plot.nnet ###
-#   #import the function from Github
-#   source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
-#   plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
+  #import the function from Github
+  source_url('https://gist.githubusercontent.com/Peque/41a9e20d6687f2f3108d/raw/85e14f3a292e126f1454864427e3a189c2fe33f3/nnet_plot_update.r')
+  par(mfrow=c(1,1))
+  plot.nnet(ann.model, alpha.val = 0.5, circle.col = list('lightgray', 'white'), bord.col = 'black')
 
   return(ann.model)
 }
 
-predict.bearing <- function (turbine, yr,start) {
+predict.bearing <- function (turbine, yr,start,end) {
 #   
-#   turbine='WTG01' 
-#   yr='2014'
-#   start=0
+#    turbine='WTG04' 
+#    yr='2010'
+#    start=0
 #   end=10000
   library(zoo)
   library(ggplot2)
   library(scales)
   library(reshape2)
 
-  predict.data<-dataloading(turbine,yr,start)
+  predict.data<-dataloading(turbine,yr,start,end)
   predict.expected.output<-predict.data$bearing.temp
   predict.data<-predict.data[,1:4]
   predict.nnet.result<-data.frame(predict(ann.model, newdata=predict.data,type="raw"))
   predict.residual<-predict.expected.output-predict.nnet.result
   #Add calculated moving averages to existing data frame
-  output <- cbind(predict.data[,1],predict.residual,rollmean(predict.residual, 100,fill = list(NA, NULL, NA)),predict.expected.output,predict.nnet.result)
-  colnames(output) <- c('Time','Residual','amb.av','Measured','Predicted')
-#   print(output)
+  output <- cbind(predict.data[,1],predict.residual,rollmean(predict.residual, 50,fill = list(NA, NULL, NA)),predict.expected.output,predict.nnet.result,rollmean(((predict.expected.output-70)>0),3,fill = list(NA, NULL, NA)),rollmean(((predict.residual-5)>0),3,fill = list(NA, NULL, NA)))
+  colnames(output) <- c('Time','Residual','amb.av','Measured','Predicted','Measured.Count','Residual.Count')
+  #count <- cbind(predict.data[,1],)
+  #  colnames(count) <- c('Time','Measured','Residual')
+  #  plot(MTime,output)
+  par(mfrow=c(2,1))
+  plot(Measured.Count~Time,output)
+  plot(Residual.Count~Time,output)
+  #   print(output)
   
   # plotting
   par(mfrow=c(2,1))
   #todo change point to date
   plot(output$Time,output$Predicted,type="l",col="red",xlab='date',ylab='bearing temperture',ylim=range(40,80),main=paste(turbine,'  from',output$Time[1],'to',output$Time[length(output$Time)]))
   lines(output$Time,output$Measured,col="green")
-  plot(output$Residual,type="p",xlab='sample point',ylab='residual',pch=20)#,ylim=range(-10,10))
+  plot(output$Time,output$Residual,type="p",xlab='date',ylab='residual',pch=20,ylim=range(-15,15))
   par(fig=c(0.65,1.0,0,0.5),new=TRUE)
-  boxplot(output$Residual,axes=FALSE,ylim=range(-10,10))
+  boxplot(output$Residual,axes=FALSE,ylim=range(-15,15))
   
   
   #Make zoo object of data
@@ -99,10 +91,11 @@ predict.bearing <- function (turbine, yr,start) {
 
 }
 
-ann.model<-bearing.training('WTG04','2009',1)
-WTG04.Output2010<-predict.bearing(turbine='WTG04', yr='2010',start=1)
-WTG04.Output2011<-predict.bearing(turbine='WTG04', yr='2011',start=1)
-WTG04.Output2012<-predict.bearing(turbine='WTG04', yr='2012',start=1)
-WTG04.Output2013<-predict.bearing(turbine='WTG04', yr='2013',start=1)
-WTG04.Output2014<-predict.bearing(turbine='WTG04', yr='2014',start=1)
+ann.model<-bearing.training('WTG09','2009',1)
+WTG09.Output2009<-predict.bearing(turbine='WTG09', yr='2009',start=1)
+WTG09.Output2010<-predict.bearing(turbine='WTG09', yr='2010',start=1)
+WTG09.Output2011<-predict.bearing(turbine='WTG09', yr='2011',start=1)
+WTG09.Output2012<-predict.bearing(turbine='WTG09', yr='2012',start=1)
+WTG09.Output2013<-predict.bearing(turbine='WTG09', yr='2013',start=1)
+WTG04.Output2014<-predict.bearing(turbine='WTG04', yr='2014',start=20000,11000)
   
